a279a528af0b7ea120ef68b18d26d538b6859665
yjf <1374993958@qq.com>
2020-06-15 15:52:59 +0800

    maven

---
+++ src/main/java/com/yjf/jekins/JekinsApplication.java
@@ -0,0 +1,13 @@
+package com.yjf.jekins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JekinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JekinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yjf/jekins/JekinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjf.jekins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JekinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
