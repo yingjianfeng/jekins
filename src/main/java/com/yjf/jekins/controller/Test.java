18e9f2bbadfeaa1fff7c9082ca79dc545d62680f
yjf <1374993958@qq.com>
2020-06-16 15:35:25 +0800

    hook

--- src/main/java/com/yjf/jekins/controller/Test.java
+++ src/main/java/com/yjf/jekins/controller/Test.java
@@ -9,7 +9,7 @@ public class Test {
     @RequestMapping("test")
     public String get(){
         System.out.println("--------in--------");
-        return "--------test success-------";
+        return "hook";
     }
 
 
